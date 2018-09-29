70a1d3ee494fa2db6f043633cd71e1cfb584aada
Josh Carroccia <agr.carroccia@gmail.com>
2018-09-29 00:02:26 -0400

    added more projects

--- script.js
+++ script.js
@@ -15,16 +15,28 @@
 var aboutMe = document.getElementById("about")
 var projects = document.getElementById("projects")
 var footer = document.getElementById("contact")
+topContainer = document.getElementById("top-container")
+
 function aboutMeSlide() {
   aboutMe.classList.remove("hide");
   footer.classList.remove("hide");
-  aboutMe.classList.add("onload-slide");
+  aboutMe.classList.add("onload-slideLeft");
   projects.classList.add("hide");
+  topContainer.classList.add("hide");
 }
 
 function projectsSlide() {
   projects.classList.remove("hide");
   footer.classList.remove("hide");
-  projects.classList.add("onload-slide");
+  projects.classList.add("onload-slideRight");
   aboutMe.classList.add("hide");
+  topContainer.classList.add("hide");
+}
+
+function contactsSlide() {
+  topContainer.classList.remove("hide");
+  footer.classList.add("hide");
+  topContainer.classList.add("onload-slideRight");
+  aboutMe.classList.add("hide");
+  projects.classList.add("hide");
 }
